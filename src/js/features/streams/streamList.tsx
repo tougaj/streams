7fcab3287a5f63f54362bf5ce9c5e74fada6c6f6
tougaj <sashamolodec@gmail.com>
2022-05-21 15:37:22 +0300

    Виправлена помилка fetch

--- src/js/features/streams/streamList.tsx
+++ src/js/features/streams/streamList.tsx
@@ -1,27 +1,7 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
-import { showSystemError } from '../../alerts';
-import { DEFAULTS } from '../../init';
+import React from 'react';
 
 interface IStreamListProps extends React.AllHTMLAttributes<HTMLDivElement> {}
 const StreamList = ({}: IStreamListProps) => {
-	useEffect(() => {
-		axios(`${DEFAULTS.streamServer.address}:${DEFAULTS.streamServer.apiPort}/v1/paths/list`, {
-			method: 'GET',
-			withCredentials: true,
-			credentials: 'same-origin',
-			mode: 'no-cors',
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-				'Content-Type': 'application/json',
-			},
-		})
-			.then((data: any) => {
-				console.log(data);
-			})
-			.catch(showSystemError);
-	}, []);
-
 	return <div className="">StreamList</div>;
 };
 
